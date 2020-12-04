d6113c9522fc644fd8ef907738c167a58658377a
swapnilkusumwal <43251574+swapnilkusumwal@users.noreply.github.com>
2020-12-05 02:08:16 +0530

    Updates

---
+++ precache-manifest.b028caf737132cecadc3f2ed846d1a32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8529612ab9f766f8ee78a8a70ca20ab",
+    "url": "/premvidyapeeth-ngo/index.html"
+  },
+  {
+    "revision": "9aa7799c067e2a2b8362",
+    "url": "/premvidyapeeth-ngo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c2806dd725b307a0ffac",
+    "url": "/premvidyapeeth-ngo/static/js/2.602fabeb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/premvidyapeeth-ngo/static/js/2.602fabeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa7799c067e2a2b8362",
+    "url": "/premvidyapeeth-ngo/static/js/main.f1f836bf.chunk.js"
+  },
+  {
+    "revision": "cf1508a151b6ca7041e0",
+    "url": "/premvidyapeeth-ngo/static/js/runtime-main.6f6ac5d2.js"
+  }
+]);
